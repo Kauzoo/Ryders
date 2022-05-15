936545b503c8b69b39422d28bb7f0f9871bc33ee
Jan Meyering <jan.meyering@tum.de>
2022-05-15 12:18:29 +0200

    idk

--- Assets/Scripts/PlayerBehaviour/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour/PlayerBehaviour.cs
@@ -6,8 +6,16 @@ using Enums;
 
 namespace Player
 {
+    /// <summary>
+    /// Example for an implementation of an PlayerBehaviour.
+    /// The base class PlayerBehaviourBase contains a default Implementation,
+    /// for all requeired methods. These can be overwritten.
+    /// This script should be attached to the Gear. (i.e. custom code is Gear based).
+    /// Custom code for Characters is not planned so far.
+    /// Input is passed in from a central InputModule.
+    /// </summary>
     public class PlayerBehaviour : PlayerBehaviourBase
     {
-        
+
     }
 }
