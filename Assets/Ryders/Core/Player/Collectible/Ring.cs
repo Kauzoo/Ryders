3a42f59841dfb2b96263beab90860385baf95150
nyr <kauzoo@web.de>
2022-06-24 23:48:05 +0200

    WIP: Rings

---
+++ Assets/Ryders/Core/Player/Collectible/Ring.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using UnityEditor;
+using UnityEngine;
+
+namespace Ryders.Core.Player.Collectible
+{
+    [RequireComponent(typeof(Collider))]
+    public class Ring : MonoBehaviour
+    {
+        [Header("Settings"), Tooltip("Toggle Ring as respawning")]
+        public bool respawn;
+        [Tooltip("Respawn Timer in Seconds")]
+        public float respawnTimer;
+
+        private float _respawnTimer;
+
+        private Collider _collider;
+
+        private void OnCollisionEnter(UnityEngine.Collision collision)
+        {
+            gameObject.SetActive(false);
+            StartCoroutine(Respawn());
+        }
+        
+        private IEnumerator Respawn()
+        {
+            yield return new WaitForSecondsRealtime(respawnTimer);
+            gameObject.SetActive(true);
+        }
+    }
+}
--- Assets/Ryders/Core/Player/Collision/RydersCollision.cs
+++ Assets/Ryders/Core/Player/Collision/RydersCollision.cs
@@ -1,9 +1,24 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Ryders.Core.Player.Collision
 {
+    // TODO consider doing this with tags
+    [RequireComponent(typeof(Collider))]
     public abstract class RydersCollision : MonoBehaviour
     {
+        protected Collider collider;
+
+        public void Start()
+        {
+            collider = GetComponent<Collider>();
+        }
         
+        
+    }
+
+    public enum BonkType
+    {
+        SoftBonk, HardBonk, CarBonk, Wall
     }
 }
