b21189be2c181f5ceebba0ae2deac56a86895513
nyr <kauzoo@web.de>
2022-07-06 16:01:12 +0200

    Addendum: SmotherCamera

--- Assets/Ryders/Core/Player/DefaultBehaviour/Visuals/PlayerCameraBehaviour.cs
+++ Assets/Ryders/Core/Player/DefaultBehaviour/Visuals/PlayerCameraBehaviour.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using System;
+using UnityEngine;
+
+namespace Ryders.Core.Player.DefaultBehaviour.Visuals
+{
+    public class PlayerCameraBehaviour : MonoBehaviour
+    {
+        public Transform playerTransform;
+
+        private Vector3 offset;
+        
+        private void Start()
+        {
+            offset = transform.localPosition;
+            transform.SetParent(null);
+        }
+
+        private void FixedUpdate()
+        {
+            transform.position = Vector3.Lerp(transform.position, playerTransform.TransformPoint(offset), 0.9f);
+            var oldRot = transform.rotation;
+            transform.LookAt(playerTransform);
+            var newRot = transform.rotation;
+            transform.rotation = oldRot;
+            transform.rotation = Quaternion.Lerp(oldRot, newRot, 0.4f);
+        }
+    }
+}
--- Assets/Ryders/Core/Player/DefaultBehaviour/Visuals/VisualPlayerBehaviour.cs
+++ Assets/Ryders/Core/Player/DefaultBehaviour/Visuals/VisualPlayerBehaviour.cs
@@ -1,9 +1,23 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Ryders.Core.Player.DefaultBehaviour.Visuals
 {
     public class VisualPlayerBehaviour : MonoBehaviour
     {
-        
+        public Transform playerTransform;
+
+        private void Start()
+        {
+            transform.SetParent(null);
+            transform.position = playerTransform.position;
+            transform.rotation = playerTransform.rotation;
+        }
+
+        private void FixedUpdate()
+        {
+            transform.position = playerTransform.position;
+            transform.rotation = Quaternion.Lerp(transform.rotation, playerTransform.rotation, 0.5f);
+        }
     }
 }
